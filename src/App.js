f267d44cedb6137c1efa14102217a0c4b58ee684
Deinyefa <deinye@webdrive.ca>
2019-05-20 11:51:49 -0400

    Project setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I am a React App!</h1>
+        <p>What's up?</p>
+      </div>
+    );
+  }
+}
+
+export default App;
