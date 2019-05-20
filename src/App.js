f5b437b65dccf889f6a47708ba790828a06b7a4c
Deinyefa <deinye@webdrive.ca>
2019-05-20 12:38:18 -0400

    rolled back to class based App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import Person from './Person/Person';
 
-const App = (props) => {
-  const [personsState, setPersonsState] = useState({
-    // state = {
+class App extends Component {
+  state = {
     persons: [
       { name: 'Fiyin', age: 22 },
       { name: 'Tobi', age: 29 },
       { name: 'Toyin', age: 27 },
       { name: 'Tolu', age: 26 }
     ]
-  });
+  }
 
-  const switchNameHandler = () => {
-    setPersonsState({
+  switchNameHandler = () => {
+    this.setState({
       persons: [
         { name: 'Fiyinfoluwa', age: 23 },
         { name: 'Tobi', age: 29 },
@@ -25,22 +24,19 @@ const App = (props) => {
     })
   }
 
-  return (
-    <div className="App">
-      <h1>Hi, I am a React App!</h1>
-      <p>What's up?</p>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
-      <Person name={personsState.persons[3].name} age={personsState.persons[3].age} />
-    </div>
-  );
-
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I am a React App!</h1>
+        <p>What's up?</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <Person name={this.state.persons[3].name} age={this.state.persons[3].age} />
+      </div>
+    );
+  }
 }
 
 export default App;
-
-
-
-
