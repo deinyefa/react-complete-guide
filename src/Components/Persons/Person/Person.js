ec4ce9c6344e608fc98a8f5d13e15b80bc9cefce
Deinyefa <deinye@webdrive.ca>
2019-05-22 08:00:48 -0400

    useContext for functional components

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext } from 'react';
 import PropTypes from 'prop-types';
 
 import PersonClass from './Person.css';
@@ -6,11 +6,21 @@ import withClass from '../../../hoc/WithClass';
 import AuthContext from '../../../context/auth-context';
 
 const Person = (props) => {
+
+  // needs to be a class-based component tho!
+  //  static contextType = AuthContext;
+  // Will let you access context in life cycle methods ex. componentDidMount
+  //  componentDidMount = () => {
+  //    console.log(this.context.authenticated)
+  //  }
+
+  const authContext = useContext(AuthContext);
+  
+
   return (
     <Fragment>
-      <AuthContext.Consumer>
-        {(context) => context.authenticated ? <p>Authenticated!</p> : <p>Please Log in</p>}
-      </AuthContext.Consumer>
+        { authContext.authenticated ? <p>Authenticated!</p> : <p>Please Log in</p>}
+        {/*OR -> { this.context.authenticated ? <p>Authenticated!</p> ... } */}
         <p onClick={props.click}>My name is {props.name} and I am {props.age} years old</p>
         <p>{props.children}</p>
         <input type="text" onChange={props.changed} defaultValue={props.name} />
